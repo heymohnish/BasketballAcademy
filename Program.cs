ce5522960d22eb1a26f00c933f76d67afc143dbc
ELANGOVAN D <103368193+Its-elango@users.noreply.github.com>
2024-08-06 17:32:54 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,9 +18,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
 app.UseCors("AllowAnyOrigin");
